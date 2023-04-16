99788e98fe8f907bf87845383e09d8049cf23c82
Estefania Ronquillo <feyssirm@gmail.com>
2023-04-16 05:57:07 -0500

    Tailwind config ready

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -39,7 +39,7 @@ const Register = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Register</h1>
+      <h1 className='text-10xl text-red-700'>Register</h1>
       <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
       <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
